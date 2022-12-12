f3ee3775979ab2e51cf252f3c716b1a120c80a34
WooriSunoo <cherry21004@gmail.com>
2022-12-12 15:16:22 +0900

    feat: Todo리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">My Todo App</div>;
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: "My Todo1",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "My Todo2",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "My Todo3",
+      done: true,
+    },
+  ]);
+  return (
+    <div className="App">
+      {todoItems.map((todoItem) => {
+        console.log(todoItem); //{id: 1, title: 'My Todo1', done: false}
+        return <Todo key={todoItem.id} todoItem={todoItem} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Todo.js
@@ -0,0 +1,22 @@
+// 1. 함수형 컴포넌트
+// 2.input(Checkbox)와 label을 렌더링하는 컴포넌트!
+// 3. App (부모 컴포넌트)에서 Todo(자식 컴포넌트) 1개를 렌더링
+
+const Todo = ({ todoItem }) => {
+  console.log("{todoItem}", todoItem); //{id: 1, title: 'My Todo1', done: false}
+  const { id, title, done } = todoItem;
+  return (
+    <div className="Todo">
+      <input
+        type="checkbox"
+        name={`todo${id}`}
+        id={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={done}
+      />
+      <label htmlFor={`todo${id}`}>{title}</label>
+    </div>
+  );
+};
+
+export default Todo;
