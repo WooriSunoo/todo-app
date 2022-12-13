9454e4af292c72592fd1777d6f02db5067a42d05
WooriSunoo <cherry21004@gmail.com>
2022-12-13 12:01:10 +0900

    feat: Todo 삭제 기능 구현

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,20 +1,25 @@
-// 1. 함수형 컴포넌트
-// 2.input(Checkbox)와 label을 렌더링하는 컴포넌트!
-// 3. App (부모 컴포넌트)에서 Todo(자식 컴포넌트) 1개를 렌더링
+import { useState } from "react";
 
-const Todo = ({ item }) => {
-  console.log("{todoItem}", item); //{id: 1, title: 'My Todo1', done: false}
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
   const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
+
   return (
     <div className="Todo">
       <input
         type="checkbox"
-        name={`todo${id}`}
         id={`todo${id}`}
+        name={`todo${id}`}
         value={`todo${id}`}
         defaultChecked={done}
       />
       <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
