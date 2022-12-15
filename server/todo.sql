52ec7a01502e1f676e3f6d51f5ab49b8d4c67ec0
WooriSunoo <cherry21004@gmail.com>
2022-12-15 09:57:29 +0900

    feat: READ API 연결

--- server/todo.sql
+++ server/todo.sql
@@ -20,3 +20,5 @@ INSERT INTO todo (title, done) VALUES ('sean todo6', 0);
 
 -- 아래방식으로 insert도 가능 auto increment 에서는 null
 INSERT INTO todo VALUES (null, 'my todo1', 0);
+
+INSERT INTO todo (title, done) VALUES ('sean todo9', 0);
